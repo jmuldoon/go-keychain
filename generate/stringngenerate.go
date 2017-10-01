d85eaae6537d4807c9c76b172d566b1dc5d6aac2
James Muldoon <james.muldoon@nyu.edu>
2017-10-01 15:06:30 +0800

    finally added the generator T_T, forgot to add

---
+++ generate/stringngenerate.go
@@ -0,0 +1,32 @@
+package generate
+
+import (
+	"math/rand"
+)
+
+const charBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_-"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+// RandStringBytesMaskImprSrc generates a masked byte string based on src input
+// pretty good implementation found by icza on stackoverflow modified for use.
+func RandStringBytesMaskImprSrc(src rand.Source, n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(charBytes) {
+			b[i] = charBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
